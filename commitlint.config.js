8f36335eb4c29dd50f974405d5d34edfb71ca291
shubhamdev98 <shubham.pn.1998@gmail.com>
2025-10-15 20:54:13 +0530

    feat(husky): configure eslint and add extra husky feature

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -7,5 +7,7 @@ module.exports = {
       ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore'],
     ],
     'subject-case': [0, 'never', []],
+    'header-max-length': [0], // Disable max length check
+    'type-case': [0], // Disable type case check
   },
-}
+};
