03c4c3e258be2c898ac67b8fcdad652d945e4daf
shubhamdev98 <shubham.pn.1998@gmail.com>
2025-10-16 11:33:42 +0530

    chore(husky): run lint-staged in pre-commit hook

--- src/data/app_data/data.ts
+++ src/data/app_data/data.ts
@@ -148,6 +148,14 @@ export const movies: Movie[] = [
     votes: '15.2K',
     img: m5,
   },
+  {
+    id: 6,
+    title: 'From the World of John Wick: Ballerina',
+    genre: 'Action/Thriller',
+    rating: 8.7,
+    votes: '15.2K',
+    img: m5,
+  },
 ]
 
 export const allMovies: Movie[] = [
